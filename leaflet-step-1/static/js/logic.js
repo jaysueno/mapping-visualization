e63f7c230707d72690a1b750990bc540bb771472
Jay Sueno <jay.sueno@gmail.com>
2020-10-07 17:40:44 -0700

    developing map

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -4,13 +4,18 @@
 // Create a leaflet map object using "L.map()"
 var myMap = L.map("map", {
     center: [37.09, -95.71],
-    zoom: 5
+    zoom: 3
+    // layers: [lightmap]
 });
+
+var baseMaps = {
+    "Light Map": lightmap
+};
 
 // Add a tile layer using "L.tilelayer()" 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 12,
+    tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
@@ -35,6 +40,23 @@ function markerSize(magnitude) {
     return magnitude * 30000;
 }
 
+// This function will return a color based on a number. It will be used to generate marker colors based on the magnitude of the earthquake
+var markerColor = function(value) {
+    if (value < 0) {
+            return '#F4ECF7'
+    } else if (value < 20) {
+            return '#D2B4DE'
+    } else if (value < 60) {
+            return '#A569BD'
+    } else if (value < 120) {
+            return '#7D3C98'
+    } else if (value < 180) {
+            return '#4B7282'
+    } else {
+            return '#4A235A'
+    } 
+}
+
 // Use D3 promise to call the data get a response to populate the markers on the map
 d3.json(url).then(function(data) {
     // Loop through locations and create city and state markers
@@ -47,30 +69,20 @@ d3.json(url).then(function(data) {
         var mag = data.features[i].properties.mag;
         var depth = data.features[i].geometry.coordinates[2];
         var place = data.features[i].properties.place;
-        console.log(place)
-
-        // Set the color fill based on the depth of earthquake
-        var fill = "";
-        if (depth > 90) {
-            fill = "red"
-        }
-        else if (depth >70) {
-            fill = "orange"
-        }
-        else if (depth > 30) {
-            fill = "yellow"
-        }
-        else {
-            fill = "green"
-        }
+        // console.log(earthquakeData[i].geometry.coordinates)
+        // console.log(lat,lon)
+        // console.log(place)
 
         // Set the marker radius
         L.circle(latlon, {
-            stroke: false,
+            stroke: "black",
             fillOpacity: 0.75, 
-            color: fill,
-            fillColor: fill,
-            radius: markerSize(mag)
+            color: '#FDFEFE',
+            fillColor: markerColor(depth),
+            fillOpacity: .6,
+            weight: 1,
+            // set marker size to reflect the magnitude
+            radius: mag * 25000
         }).addTo(myMap);
 
         
@@ -78,11 +90,41 @@ d3.json(url).then(function(data) {
 })
 
 // Create Legend
-var legend = L.control({position: "bottomright"});
+var legend = L.control({
+    position: "bottomright"
+});
+
 legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "Info Legend");
-    var depth_label = [-10, 10, 30, 50, 70, 90];
-    return div
+    var div = L.DomUtil.create("div", "legend");
+    var limits = [
+        '<0',
+        '0 - 19',
+        '20 - 59',
+        '80 - 119',
+        '120 - 179',
+        '>180'
+    ];
+    var colors = [
+            '#F4ECF7',
+            '#D2B4DE',
+            '#A569BD',
+            '#7D3C98',
+            '#4B7282',
+            '#4A235A'
+    ]; 
+
+    // Legend heading
+    var legendInfo = "<h3>Earthquake Depth</h3>"
+
+    div.innerHTML = legendInfo;
+
+    for (var i = 0; i < limits.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + (colors[i]) + '"></i> ' +
+                limits[i] + '<br>';
+        }
+
+    return div;
 };
 
 legend.addTo(myMap)
